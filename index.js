b7fb397f9df084e53fc584794ec184083bc2cc96
Tien Tien Yep <veronica_yep@hotmail.com>
2020-07-16 00:28:58 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
 const bodyParser = require("body-parser");
-const util = require("util");
 const mysql = require("mysql");
 // const dotenv = require("dotenv").config();
 
